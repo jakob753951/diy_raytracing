b0ec23f87ddc1d311cbb2ef67285698206ab58d0
Jakob Ladegaard Møller <jlm1999@live.dk>
2026-01-17 11:25:19 +0100

    remove unused `hit_sphere` function in sphere.rs.

--- src/sphere.rs
+++ src/sphere.rs
@@ -35,17 +35,3 @@ impl Hittable for Sphere {
         Some(Hit { p, normal, t, })
     }
 }
-
-fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> f64 {
-    let oc = center - ray.origin;
-    let a = ray.direction.length_squared();
-    let h = Vec3::dot(&ray.direction, &oc);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = h * h - a * c;
-
-    if discriminant < 0. {
-        -1.0
-    } else {
-        (h - discriminant.sqrt()) / a
-    }
-}
